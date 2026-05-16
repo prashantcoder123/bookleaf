0a670a68a613c2a1c2070f91c444dc782cd8afa3
prashant kumar <prashantkum7676@gmail.com>
2026-05-16 20:03:54 +0530

    Deployment configs added

--- Frontend/src/socket/socket.js
+++ Frontend/src/socket/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socketURL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+const socket = io(socketURL);
 
 export default socket;
